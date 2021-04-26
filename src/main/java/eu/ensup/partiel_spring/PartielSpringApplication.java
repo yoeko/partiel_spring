b0c37ff41bfe195b487bebad903116d4a3c700a2
yoeko <jklu@zenetys.com>
2021-04-26 22:57:07 +0200

    Update application main file

--- src/main/java/eu/ensup/partiel_spring/PartielSpringApplication.java
+++ src/main/java/eu/ensup/partiel_spring/PartielSpringApplication.java
@@ -2,6 +2,8 @@ package eu.ensup.partiel_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PartielSpringApplication {
@@ -9,5 +11,10 @@ public class PartielSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PartielSpringApplication.class, args);
 	}
+	
+//	@Bean 
+//	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//	    return new BCryptPasswordEncoder(); 
+//	}
 
 }
